122cca87072f46f020e233e0b06bed044ed2681a
Abderrahmane Sebban <asebban@e3r10p16.1337.ma>
2025-07-08 17:41:28 +0100

    handle the case if user use big map

--- raycaster/mini_map.c
+++ raycaster/mini_map.c
@@ -6,12 +6,15 @@
 /*   By: asebban <asebban@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/08 10:04:45 by asebban           #+#    #+#             */
-/*   Updated: 2025/07/08 11:46:16 by asebban          ###   ########.fr       */
+/*   Updated: 2025/07/08 17:39:34 by asebban          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3d.h"
 
+
+#define MINI_MAP_WINDOW_WIDTH  20
+#define MINI_MAP_WINDOW_HEIGHT 20
 #define MINI_TILE_SIZE 5
 #define MINI_MAP_OFFSET_X 10
 #define MINI_MAP_OFFSET_Y 10
@@ -44,27 +47,42 @@ void draw_minimap_player(t_game *game)
 }
 
 
-void    draw_minimap(t_game *game)
+
+void draw_minimap(t_game *game)
 {
+    int player_tile_x;
+    int player_tile_y;
 
-    int y;
-    int x;
+    player_tile_x = (int)game->player.pos.x;
+    player_tile_y = (int)game->player.pos.y;
+    int window_start_x = player_tile_x - MINI_MAP_WINDOW_WIDTH / 2;
+    int window_start_y = player_tile_y - MINI_MAP_WINDOW_HEIGHT / 2;
 
-    y = 0;
-    while (y < game->map_height)
+    int y = 0;
+    int x = 0;
+    while (y < MINI_MAP_WINDOW_HEIGHT)
     {
         x = 0;
-        while (x < game->map_width)
+        while (x < MINI_MAP_WINDOW_WIDTH)
         {
-            if (game->map[y][x] == '1')
+            int map_x = window_start_x + x;
+            int map_y = window_start_y + y;
+
+            if (map_x >= 0 && map_x < game->map_width &&
+                map_y >= 0 && map_y < game->map_height &&
+                game->map[map_y][map_x] == '1')
             {
                 int sx = MINI_MAP_OFFSET_X + x * MINI_TILE_SIZE;
                 int sy = MINI_MAP_OFFSET_Y + y * MINI_TILE_SIZE;
-                draw_square(&game->screen, sx, sy, MINI_TILE_SIZE, 0xFFFFFF);// 0xFFFFFF white
+
+                draw_square(&game->screen, sx, sy, MINI_TILE_SIZE, 0xFFFFFF);
             }
             x++;
         }
         y++;
     }
-    draw_minimap_player(game);
+    int px = MINI_MAP_OFFSET_X + (MINI_MAP_WINDOW_WIDTH / 2) * MINI_TILE_SIZE;
+    int py = MINI_MAP_OFFSET_Y + (MINI_MAP_WINDOW_HEIGHT / 2) * MINI_TILE_SIZE;
+
+    draw_square(&game->screen, px - 2, py - 2, 4, 0xFF0000);
 }
