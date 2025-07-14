7dcd0d23031d7d2ab6dc2f44e201cb271bc54db4
Hamza Falati <hfalati@e3r11p4.1337.ma>
2025-07-14 17:22:24 +0100

    fix norm in main

--- mandatory/src/main.c
+++ mandatory/src/main.c
@@ -12,25 +12,6 @@
 
 #include "../includes/cub3d.h"
 
-int	mouse_move(int x, int y, t_game *game)
-{
-	static int	last_x = -1;
-	int			delta_x;
-
-	(void)y;
-
-	if (last_x == -1)
-		last_x = x;
-
-	delta_x = x - last_x;
-	rotate_player(game, delta_x * 0.003);
-
-	last_x = x;
-
-	return (0);
-}
-
-
 int	main(int ac, char **argv)
 {
 	t_parsing	p;
@@ -42,23 +23,17 @@ int	main(int ac, char **argv)
 		return (EXIT_FAILURE);
 	}
 	if (!ft_parsing(argv[1], &p))
-	{
-		print_error("Error :Parsing failed\n");
-		return (EXIT_FAILURE);
-	}
+		return (print_error("Error :Parsing failed\n"), EXIT_FAILURE);
 	if (!ft_init_game(&g, &p))
 	{
 		print_error("Error :Game initialization failed\n");
-		cleanup_game(&g);
-		return (EXIT_FAILURE);
+		return (cleanup_game(&g), EXIT_FAILURE);
 	}
-	mlx_hook(g.win, 2, 1L<<0, key_press, &g);
-	mlx_hook(g.win, 3, 1L<<1, key_release, &g);
-	mlx_hook(g.win, 17, 1L<<17, close_game, &g);
-	mlx_hook(g.win, 6, 1L<<6, mouse_move, &g);
+	mlx_hook(g.win, 2, 1L << 0, key_press, &g);
+	mlx_hook(g.win, 3, 1L << 1, key_release, &g);
+	mlx_hook(g.win, 17, 1L << 17, close_game, &g);
 	mlx_loop_hook(g.mlx, ft_game_loop, &g);
 	mlx_loop(g.mlx);
-
 	cleanup_game(&g);
 	return (0);
 }
